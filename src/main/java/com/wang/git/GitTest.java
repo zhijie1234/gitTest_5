b2f486df15dd48d00cbc9a5465b7527361681d3a
zhijie <zhijie@163.com>
2022-09-13 18:52:48 +0800

    master first commit

---
+++ src/main/java/com/wang/git/GitTest.java
@@ -0,0 +1,10 @@
+package com.wang.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("this is a git-test");
+        System.out.println("this is a git-test");
+        System.out.println("this is a git-test");
+        System.out.println("this is a git-test");
+    }
+}
