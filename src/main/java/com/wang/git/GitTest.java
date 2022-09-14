cee5f4491eb510c06b38c4befbc0dc4866e35c20
zhijie1234 <113358904+zhijie1234@users.noreply.github.com>
2022-09-14 15:00:28 +0800

    Update GitTest.java

--- src/main/java/com/wang/git/GitTest.java
+++ src/main/java/com/wang/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("this is a git-test");
         System.out.println("master commit_test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
